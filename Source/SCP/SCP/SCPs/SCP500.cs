d8e53f44e7e6f23bbe0b7dc24b093dda3e00cca0
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-23 15:53:59 -0800

    500 framework in place

--- Source/SCP/SCP/SCPs/SCP500.cs
+++ Source/SCP/SCP/SCPs/SCP500.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
 
 namespace SCP
 {
-    class SCP500
+    class HediffComp_CureDisease : HediffComp
     {
 
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            // tick: reduce severity of all diseases
+        }
+
+        public override void CompPostPostRemoved()
+        {
+            // remove all remaining diseases if not cured
+        }
+    }
+    public class HediffCompProperties_CureDisease : HediffCompProperties
+    {
+        public float severityReduction;
+        public int ticks;
+
+        public HediffCompProperties_CureDisease()
+        {
+            base.compClass = typeof(HediffComp_CureDisease);
+        }
     }
 }
