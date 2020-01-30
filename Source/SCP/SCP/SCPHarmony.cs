35df2e376ef1cf3d040f58bcc642e6249205b6a2
Smash Phil <supersmashphil@gmail.com>
2020-01-30 10:35:37 -0700

    finalized push

--- Source/SCP/SCP/SCPHarmony.cs
+++ Source/SCP/SCP/SCPHarmony.cs
@@ -105,10 +105,12 @@ namespace SCP
         {
             if(__result == WorldPawnSituation.Free && p.kindDef == PawnKindDefOf_SCP.SCP_939_PawnKindDef)
             {
+                Log.Message("Checking " + p.LabelShort);
                 foreach(Map map in Find.Maps)
                 {
                     if(map.GameConditionManager.ActiveConditions.Any(x => x is GameCondition_SCP939 && (x as GameCondition_SCP939).ActiveSCPInArea.Contains(p)))
                     {
+                        Log.Message("Result Changed ");
                         __result = WorldPawnSituation.InTravelingTransportPod;
                         return;
                     }
